0cf4a84f7408b7ef6d253c019adec9e5dd9b4233
danf00 <daniel.feige08@icloud.com>
2026-08-11 21:53:08 +0200

    README update

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 
+# das mnsit datenset laden und die richtige struktur bringen
+
 df = pd.read_csv('archive/mnist_train.csv')
 
 data = df.values
@@ -10,12 +12,15 @@ X_train = data[:, 1:]
 
 X_train = X_train / 255.0
 
+# bias und weights startwerte festlegen
+
 W1 = np.random.randn(784, 128) * 0.01
 b1 = np.zeros((1, 128))
 W2 = np.random.randn(128, 10) * 0.01
 b2 = np.zeros((1, 10))
 
 def relu(Z):
+    """ReLu Aktivierungsfunktion"""
     return np.maximum(0, Z)
 
 def softmax(z):
@@ -32,6 +37,18 @@ def forward(X, W1, b1, W2, b2):
 
 Z1, A1, Z2, A2 = forward(X_train, W1, b1, W2, b2)
 
-print(A2.shape)
-print(np.sum(A2, axis=1))
-print(A2[0])
+def one_hot(y, num_classes = 10):
+    m = y.shape[0]
+    onehot = np.zeros((m, num_classes))
+    onehot[np.arange(m), y.astype(int)] = 1
+    return onehot
+
+def cross_entropy_loss(A2, y_onehot):
+    m = y_onehot.shape[0]
+    loss = -np.sum(y_onehot * np.log(A2)) / m
+    return loss
+
+y_onehot = one_hot(Y_train)
+loss = cross_entropy_loss(A2, y_onehot)
+
+print(loss)
