f1342911b1264ec526812675aa1598f2edf1f8c9
danf00 <daniel.feige08@icloud.com>
2026-08-11 13:02:47 +0200

    load_data_1

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('archive/mnist_train.csv')
+
+data = df.values
+
+Y_train = data[:, 0]
+X_train = data[:, 1:]
+
+X_train = X_train / 255.0
+
+print(len(Y_train))
+print(len(X_train[2]))
---
+++ test.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+x = np.random.random()
+
+arr = np.random.random(5)
+
+matrix = np.random.random((3, 4))
+
+#print(np.random.randn(784, 128) * 0.01)
+
+print(np.zeros((1, 128)))
