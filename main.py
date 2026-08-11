b798f76ddf5fc08f0b0730d410efcef451fbf553
danf00 <daniel.feige08@icloud.com>
2026-08-11 22:58:09 +0200

    Beschreibungen im code hinzugefügt

--- main.py
+++ main.py
@@ -19,14 +19,19 @@ b1 = np.zeros((1, 128))
 W2 = np.random.randn(128, 10) * 0.01
 b2 = np.zeros((1, 10))
 
+# Aktivierungsfunktion für nicht linearität
+
 def relu(Z):
-    """ReLu Aktivierungsfunktion"""
     return np.maximum(0, Z)
+
+# softmax funktion für die ausgabe der klasse als Wahrscheinlichkeit
 
 def softmax(z):
     z_shifted = z - np.max(z, axis=1, keepdims=True)
     exp_z = np.exp(z_shifted)
     return exp_z / np.sum(exp_z, axis=1, keepdims=True)
+
+# Durchlauf der Daten durch das Netzwerk
 
 def forward(X, W1, b1, W2, b2):
     Z1 = X @ W1 + b1
@@ -35,7 +40,13 @@ def forward(X, W1, b1, W2, b2):
     A2 = softmax(Z2)
     return Z1, A1, Z2, A2
 
+# Daten einspeisung
+
 Z1, A1, Z2, A2 = forward(X_train, W1, b1, W2, b2)
+
+### loss berechnung
+
+# initaliesierung eines arrays für den Vergleich der Vorhersagen mit den tatsächlichen Werten
 
 def one_hot(y, num_classes = 10):
     m = y.shape[0]
@@ -43,12 +54,17 @@ def one_hot(y, num_classes = 10):
     onehot[np.arange(m), y.astype(int)] = 1
     return onehot
 
+
+
 def cross_entropy_loss(A2, y_onehot):
+    epsilion = 1e-12
     m = y_onehot.shape[0]
-    loss = -np.sum(y_onehot * np.log(A2)) / m
+    loss = -np.sum(y_onehot * np.log(A2 + epsilion)) / m
     return loss
 
 y_onehot = one_hot(Y_train)
 loss = cross_entropy_loss(A2, y_onehot)
+
+print(A2.shape)
 
 print(loss)
